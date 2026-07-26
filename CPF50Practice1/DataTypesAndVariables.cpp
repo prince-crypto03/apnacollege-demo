c717e21a4706ffd40270aae5ded3d8c252eb62e5
prince <vekariyaprince72@gmail.com>
2026-07-26 15:31:43 +0530

    practice

---
+++ CPF50Practice1/DataTypesAndVariables.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+     // Declaring and initializing variables
+     int a = 10;
+     float b = 5.5f;
+     double c = 10.99999;
+     char d = 'A';
+     bool e = true;
+
+    //Printing The values
+    cout << "Integer value:" <<
+    a << endl;
+    cout << "float value:" <<
+    b << endl;
+    cout << "double value:" <<
+    c << endl;
+    cout << "char value:" <<
+    d << endl;
+    cout << "boll value:" <<
+    e << endl;
+    
+return 0;
+}
